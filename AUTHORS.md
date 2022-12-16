4bb4919284401ca4919127770ca92c14c916a3fc
robin <robin.oval@gmail.com>
2022-12-16 00:22:33 -0500

    [Initialization]

---
+++ AUTHORS.md
@@ -0,0 +1,3 @@
+# Authors
+
+- Robin Oval <<robin.oval@gmail.com>> [@robin-oval](https://github.com/robin-oval)
---
+++ conftest.py
@@ -0,0 +1,36 @@
+import pytest
+import compas
+import fourf
+import math
+import numpy
+
+
+def pytest_ignore_collect(path):
+    if "rhino" in str(path):
+        return True
+
+    if "blender" in str(path):
+        return True
+
+    if "ghpython" in str(path):
+        return True
+
+
+@pytest.fixture(autouse=True)
+def add_compas(doctest_namespace):
+    doctest_namespace["compas"] = compas
+
+
+@pytest.fixture(autouse=True)
+def add_fourf(doctest_namespace):
+    doctest_namespace["fourf"] = fourf
+
+
+@pytest.fixture(autouse=True)
+def add_math(doctest_namespace):
+    doctest_namespace["math"] = math
+
+
+@pytest.fixture(autouse=True)
+def add_np(doctest_namespace):
+    doctest_namespace["np"] = numpy
